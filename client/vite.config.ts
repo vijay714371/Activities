49e58f546039a672c0e8739becc33f41c724a530
vijay714371 <vijaykumar714371@gmail.com>
2025-06-01 21:33:17 +0530

    setup the project in windows system

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,8 @@ import mkcert from 'vite-plugin-mkcert';
 // https://vite.dev/config/
 export default defineConfig({
   server:{
-    port:3000
+    port:3000,
+    https: false
   },
-  plugins: [react(),mkcert()],
+  plugins: [react()],
 })
